8a76d3f922a1044260e425742a752b27188c2202
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-11-20 13:53:46 -0300

    atualização

---
+++ Estudos/session_start/login.php
@@ -0,0 +1,29 @@
+<?php
+    session_start();
+    $NOME= $_POST['nm'];
+    $SENHA = $_POST['se'];
+    if($NOME == 'Arthur' && $SENHA == '123'){
+        $_SESSION['logado'] = true;
+        header('location: menu.php');
+        exit();
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <form action="login.php" method="post">
+        <label for="nm">Name: </label>
+        <input type="text" name="nm" id="nm">
+        <label for="se">Senha: </label>
+        <input type="password" name="se" id="se">
+        <br><br>
+        <input type="submit" value="Validar">
+    </form>
+</body>
+</html>
